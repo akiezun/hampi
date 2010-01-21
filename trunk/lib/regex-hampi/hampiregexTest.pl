22a2e3123e0f5ffe156b278ea4b18ffa39d9742c
akiezun <akiezun@4d5e3db4-f335-11dd-ac65-f74f249cfaac>
2010-01-21 22:08:41 +0000

    regex to Hampi translation donated by Devdatta Akhawe devdatta@eecs.berkeley.edu

---
+++ trunk/lib/regex-hampi/hampiregexTest.pl
@@ -0,0 +1,29 @@
+#/usr/bin/perl -w
+#
+
+use strict;
+use lib '.';
+use pcre_tohampi;
+
+
+while(<>){
+  chomp;
+  my $regex=$_;
+  $regex=$1.$2 if( $regex =~ m/(\/.*?\/[^\/g]*)g([^\/g]*)/);
+  $regex=eval 'qr'.$regex;
+  my $p= pcre_tohampi->new($regex);
+  $p->parse;
+  my ($str,$min,$max)=$p->tothehampi();
+  die "error calculating minimum for regex" if $min==0;
+  open(HMP,'>','/tmp/foo.hmp') or die $!;
+  print HMP "\nvar string:$min;\n$str\nassert string in flax0;\n";
+  close(HMP);
+  open(RES,"./hampi.sh /tmp/foo.hmp |") or die $!;
+  while(<RES>){
+    chomp;
+    if(m/^\{VAR\(string\)\=(.*?)\}$/i){
+      die "Failure for $regex\n" if $1 !~ $regex;
+      print "SUCCESS for $regex\n";
+    }
+  }
+}
